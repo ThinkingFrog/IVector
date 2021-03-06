b4b1f2e368dd857b8090ace4dc6248e1ef61b407
ThinkingFrog <khripunkovdv@gmail.com>
2021-03-06 17:04:06 +0300

    Add amount counter to RC

--- include/RC.h
+++ include/RC.h
@@ -11,5 +11,6 @@ enum RC {
     ALLOCATION_ERROR, // Couldn't allocate new memory
     NULLPTR_ERROR, // Received nullptr
     FILE_NOT_FOUND, // Couldn't file with corresponding name
-    VECTOR_NOT_FOUND // Couldn't find vector inst in ISet method
+    VECTOR_NOT_FOUND, // Couldn't find vector inst in ISet method
+    AMOUNT
 };
