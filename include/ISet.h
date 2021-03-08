c99d65b06b7cfca88f88c45bba711d059500227f
Burkov Alexey <rubicks_cube@mail.ru>
2021-03-08 21:39:36 +0300

    Fixed mistakes

--- include/ISet.h
+++ include/ISet.h
@@ -21,7 +21,7 @@ public:
 	virtual RC get(size_t index, IVector const* val) const = 0;
 	virtual RC findFirst(ISet const * const& pat, IVector::NORM n, double tol, IVector const *& val) const = 0;
 
-	virtual RC insert(ISet const *& val, IVector::NORM n, double tol) = 0;
+	virtual RC insert(IVector const *& val, IVector::NORM n, double tol) = 0;
 
 	virtual RC remove(size_t index) = 0;
 	virtual RC remove(ISet const * const& pat, IVector::NORM n, double tol) = 0;
