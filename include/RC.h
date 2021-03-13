22eb40741781996eeddd58328ad916555df73dd6
ThinkingFrog <khripunkovdv@gmail.com>
2021-03-13 10:33:50 +0300

    Change comments to RC

--- include/RC.h
+++ include/RC.h
@@ -6,12 +6,12 @@ enum class RC {
     INVALID_ARGUMENT,
     MISMATCHING_DIMENSIONS,
     INDEX_OUT_OF_BOUND,
-    INFINITY_OVERFLOW,
+    INFINITY_OVERFLOW, // Number is greater than infinity
     NOT_NUMBER,
     ALLOCATION_ERROR, // Couldn't allocate new memory
     NULLPTR_ERROR, // Received nullptr
-    FILE_NOT_FOUND, // Couldn't file with corresponding name
-    VECTOR_NOT_FOUND, // Couldn't find vector inst in ISet method
-    IO_ERROR,// Couldn't write/read to/from file
+    FILE_NOT_FOUND, // Couldn't find file with corresponding name
+    VECTOR_NOT_FOUND, // Couldn't find vector instance in ISet method
+    IO_ERROR, // Couldn't write/read to/from file
     AMOUNT
 };
