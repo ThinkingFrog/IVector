3940cae2d3536fa0f4e7508473f86c97f57637d2
Burkov Alexey <rubicks_cube@mail.ru>
2021-04-03 19:54:32 +0300

    Added Problem interface

---
+++ include/IProblem.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "ICompact.h"
+#include "ILogger.h"
+#include "IVector.h"
+
+class IProblem {
+	static IProblem * createProblem(ICompact const * const &params, ICompact const * const &args);
+
+	static RC setLogger(ILogger * const logger);
+
+	virtual bool isValidParams(IVector const * const &params) const = 0;
+	virtual bool isValidArgs(IVector const * const &args) const = 0;
+
+	virtual RC setParams(IVector const * const &params) = 0;
+	virtual RC setArgs(IVector const * const &args) = 0;
+
+	virtual double evalByArgs(IVector const * const &args) const = 0;
+	virtual double evalByParams(IVector const * const &params) const = 0;
+
+	virtual ~IProblem() = 0;
+
+private:
+	IProblem(const IProblem &) = delete;
+	IProblem& operator=(const IProblem &) = delete;
+
+protected:
+	IProblem() = default;
+};
