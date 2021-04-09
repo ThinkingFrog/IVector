7357eeab8a270b146093ea15b40e3f597b705b3e
Burkov Alexey <rubicks_cube@mail.ru>
2021-04-09 15:30:10 +0300

    Added missed methods in multi-index

--- include/IMultiIndex.h
+++ include/IMultiIndex.h
@@ -1,17 +1,21 @@
 #pragma once
 #include <cstddef>
+#include "ILogger.h"
 
 class IMultiIndex {
 public:
     static IMultiIndex * createMultiIndex(size_t dim, const size_t* indices);
+    virtual IMultiIndex * clone() const = 0;
+
+    virtual size_t getDim() const = 0;
+    virtual const size_t *getData() const = 0;
+
+    static RC setLogger(ILogger * const pLogger);
 
     virtual RC getAxisIndex(size_t axisIndex, size_t &val) const = 0;
     virtual RC setAxisIndex(size_t axisIndex, size_t val) = 0;
 
     virtual RC incAxisIndex(size_t axisIndex, ssize_t val) = 0;
-
-    virtual size_t getDim() const = 0;
-    virtual const size_t *getData() const = 0;
 
     virtual ~IMultiIndex() = 0;
 
