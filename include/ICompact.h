3ea04eaa9c86a2e2b1a459d01b8edb7a54b09af8
Burkov Alexey <rubicks_cube@mail.ru>
2021-04-09 15:22:48 +0300

    Added missed methods

--- include/ICompact.h
+++ include/ICompact.h
@@ -8,13 +8,12 @@ class ICompact {
 public:
     static ICompact* createCompact(IVector const * vec1, IVector const * vec2, IMultiIndex const *nodeQuantities);
 
-    static ICompact* createIntersection(ICompact const *op1, ICompact const *op2, double tol);
-
-    /* CompactSpan - компактная оболочка: строим наименьшее компактное множество, содержащее 2 переданных */
-    static ICompact* createCompactSpan(ICompact const *op1, ICompact const *op2);
-
     virtual ICompact *clone() const = 0;
 
+    static RC setLogger(ILogger* const logger);
+
+    virtual size_t getDim() const = 0;
+    virtual IMultiIndex* getGrid() const = 0;
     /*
     * Method creating new IVector and assigning new address to val
     */
@@ -25,10 +24,25 @@ public:
     */
     virtual RC getVectorCoords(IMultiIndex const *index, IVector * const& val) const = 0;
 
+    virtual RC setLeftBoundary(IVector const * vec) = 0;
+    virtual RC setRightBoundary(IVector const * vec) = 0;
+    virtual RC setGrid(IMultiIndex const *nodeQuantities) = 0;
+
+    static ICompact* createIntersection(ICompact const *op1, ICompact const *op2, double tol);
+    /* CompactSpan - компактная оболочка: строим наименьшее компактное множество, содержащее 2 переданных */
+    static ICompact* createCompactSpan(ICompact const *op1, ICompact const *op2);
+
     class IIterator {
     public:
         virtual IIterator * getMovedIterator(IVector const *inc) = 0;
+        virtual IIterator * clone() const = 0;
+
+        static RC setLogger(ILogger * const pLogger);
+
         virtual RC moveIterator(IVector const *inc) = 0;
+
+        static bool equal(const IIterator *op1, const IIterator *op2);
+
         /*
         * Method creating new IVector and assigning new address to val
         */
@@ -41,6 +55,12 @@ public:
 
         virtual ~IIterator() = 0;
 
+    protected:
+        /*
+         * Every iterator corresponds to grid node -> corresponds to some multi-index, method necessary for comparison
+         */
+        virtual IMultiIndex const * const getIndex() const = 0;
+
     private:
         IIterator(const IIterator&) = delete;
         IIterator& operator=(const IIterator&) = delete;
@@ -52,8 +72,6 @@ public:
     IIterator * getIterator(IVector const *vec) const = 0;
     IIterator * getBegin() const = 0;
     IIterator * getEnd() const = 0;
-
-    static RC setLogger(ILogger* const logger);
 
 private:
     ICompact(const ICompact& compact) = delete;
