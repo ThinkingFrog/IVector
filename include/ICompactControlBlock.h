2f6713cff53ee182a801f3f51aa89cf414369fac
Burkov Alexey <32144882+AlexeyBurkov@users.noreply.github.com>
2021-04-23 13:53:56 +0300

    Added commentary

--- include/ICompactControlBlock.h
+++ include/ICompactControlBlock.h
@@ -4,6 +4,14 @@
 
 class ICompactControlBlock {
 public:
+    /*
+    * Control block finds closest node to vector: val + shift, and stores this node elements in val
+    * 
+    * @param [in] shift Shift, that defines changes of iterator position
+    *
+    * @param [in] val Old iterator position, new position will be stored here
+    *
+    */
     virtual RC get(IVector const * const &shift, IVector * const&val) const = 0;
 
     virtual ~ICompactControlBlock() = 0
