3c1a1dd2df60779f3c7e947b6c8f60d8d3ee5781
Burkov Alexey <32144882+AlexeyBurkov@users.noreply.github.com>
2021-04-26 20:22:10 +0300

    Update ICompactControlBlock.h

--- include/ICompactControlBlock.h
+++ include/ICompactControlBlock.h
@@ -6,14 +6,21 @@
 class ICompactControlBlock {
 public:
     /*
-    * Control block finds closest node to vector: val + shift, and stores this node elements in val
+    * Control block moves iterator forward in grid of compact. When reached last node of an axis, next axis position will be incremented. Next axis defined by bypass_order
     * 
     * @param [in] current_index Multi-index of current iterator position
     *
-    * @param [in] val Old iterator position, new position will be stored here
-    *
+    * @param [in] bypass_order Multi-index, that defining bypass orred of axis
     */
-    virtual RC get(IMultiIndex * const &current_index, IVector* const &val) const = 0;
+    virtual RC get(IMultiIndex * const &current_index, IMultiIndex const * const &bypass_order) const = 0;
+    /*
+    * Control block clculates vector corresponding to multi-index
+    * 
+    * @param [in] current_index Multi-index of current iterator position
+    *
+    * @param [in] val Buffer for vector data
+    */
+    virtual RC get(IMultiIndex const * const &current_indexIVector* const &val) const = 0;
 
     virtual ~ICompactControlBlock() = 0;
 
