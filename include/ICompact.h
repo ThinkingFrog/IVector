1f55ebcf5f4b1c46ec4c45edd154f2fad9aff000
Burkov Alexey <32144882+AlexeyBurkov@users.noreply.github.com>
2021-04-28 23:54:27 +0300

    Changed method names and added destructor

--- include/ICompact.h
+++ include/ICompact.h
@@ -37,12 +37,12 @@ public:
 
     class IIterator {
     public:
-        virtual IIterator * getMovedIterator() = 0;
+        virtual IIterator * getNext() = 0;
         virtual IIterator * clone() const = 0;
 
         static RC setLogger(ILogger * const pLogger);
 
-        virtual RC moveForwardIterator() = 0;
+        virtual RC next() = 0;
 
         /*
         * Method creating new IVector and assigning new address to val
@@ -68,6 +68,8 @@ public:
     virtual IIterator* getBegin(IMultiIndex const * const &bypassOrder) const = 0;
     // возвращает итератор на правейшую границу
     virtual IIterator* getEnd(IMultiIndex const * const &bypassOrder) const = 0;
+    
+    virtual ICompact() = 0;
 
 private:
     ICompact(const ICompact& compact) = delete;
